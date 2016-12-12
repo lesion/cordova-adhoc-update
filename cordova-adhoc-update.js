aad8de1e3f1b1613241c38108b7b72de46d850ff
lesion <lesion@autistici.org>
2016-12-12 16:28:32 +0100

    remove logs

--- cordova-adhoc-update.js
+++ cordova-adhoc-update.js
@@ -26,16 +26,13 @@ export default {
 
     return fetch(this.update_url)
       .then(function (response) {
-        console.error(response, 'RESPONSE !')
         if (response.status >= 400) throw new Error(response)
         return response.json()
       })
       .then(last_release => {
-        console.error(last_release)
         // check if remote release is newer than current one
         if (!last_release.hasOwnProperty('release')) throw new Error('`release` not found in remote manifest')
         if (!last_release.hasOwnProperty('url')) throw new Error('`url` not found in remote manifest')
-        console.error(this)
         this.release_url = last_release.url
 
         return (newestRelease(last_release.release, this.current_release) ? last_release : false)
